1fe243804dd8aa042d9cfb9b38909fabe5eb952d
Miraz Ahmed <miraz37@gmail.com>
2021-05-28 15:56:11 -0400

    Server URL error fixed

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'https://picture-stories-app.herokuapp.com' })
+const API = axios.create({baseURL: 'https://picture-stories-app.herokuapp.com/posts' })
 API.interceptors.request.use((req) =>{
     if(localStorage.getItem('profile')){
         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
