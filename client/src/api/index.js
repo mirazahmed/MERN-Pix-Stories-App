d03b9e4b60adba5be879d35c5bf233a677e090bb
Miraz Ahmed <miraz37@gmail.com>
2021-05-28 15:45:20 -0400

    new server URL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'https://mern-picture-blog.herokuapp.com/posts' })
+const API = axios.create({baseURL: 'https://picture-stories-app.herokuapp.com' })
 API.interceptors.request.use((req) =>{
     if(localStorage.getItem('profile')){
         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
